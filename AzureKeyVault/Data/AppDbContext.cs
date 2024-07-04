ef88af1125cf8776d7c43628255132a8cb36dd03
Ajit Mhatre <ajitgm99@gmail.com>
2024-07-04 14:46:46 +0530

    Add project files.

---
+++ AzureKeyVault/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using AzureKeyVault.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureKeyVault.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Employee>().HasData(new Employee
+            //{
+            //    EmployeeId = 1,
+            //    EmployeeCode = "1101",
+            //    Address = "New Work",
+            //    Designation = "Software Engineer"
+            //});
+
+            //modelBuilder.Entity<Employee>().HasData(new Employee
+            //{
+            //    EmployeeId = 2,
+            //    EmployeeCode = "1102",
+            //    Address = "New Work -USA",
+            //    Designation = "Chemical Engineer"
+            //});
+        }
+
+    }
+}
---
+++ AzureKeyVault/Readme/txt.cs
@@ -0,0 +1,16 @@
+﻿namespace AzureKeyVault.Readme
+{
+    public class txt
+    {
+
+
+        //Server=AJIT_MHATRE\\SQLEXPRESS;Database=Employee;Trusted_Connection=True;TrustServerCertificate=True
+        //Key Valut >> Crete Coupon Key Valut
+
+        //Create Secret Key >> allowed by RBAC   
+
+        // Access control >> Add role assment >> Key valut administrator >> add your role
+
+        //Create Secret Key 
+    }
+}
